ebaba6e3cd384d24959af81a1c94e321b8cd9dab
Kikkiflo <florentin.kristina@gmail.com>
2024-10-17 11:04:20 +0200

    created new files for user to join household and choose name and avatar

---
+++ components/JoinByCode.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button, Text, useTheme } from 'react-native-paper';
+import { useAppSelector } from '../store/store';
