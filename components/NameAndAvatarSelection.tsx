532ad442ca820b1cef914bc16c2bf06c1900940b
Kikkiflo <florentin.kristina@gmail.com>
2024-10-18 10:36:17 +0200

    Initial struktur NameAndAvatarSelection

---
+++ components/NameAndAvatarSelection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const NameAndAvatarSelection = () => {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+});
+
+export default NameAndAvatarSelection;
