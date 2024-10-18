b55e481fd03b3417eb98a743440fdc892e474095
FridaUltra <5carlsonfrida@gmail.com>
2024-10-18 10:42:47 +0200

    Change name

--- App.tsx
+++ App.tsx
@@ -3,20 +3,20 @@ import React, { useEffect, useState } from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { Provider as ReduxProvider } from 'react-redux';
 /* import { useAuth } from './hooks/useAuth'; */
+import { onAuthStateChanged } from 'firebase/auth';
+import { User } from './data/types';
+import { auth } from './firebase';
+import LoginStackNavigator from './navigators/LoginStackNavigator';
 import RootStackNavigator from './navigators/RootStackNavigator';
 import { AuthProvider } from './providers/AuthContextProvider';
 import store from './store/store';
-import LoginStackNavigator from './navigators/LoginStackNavigator';
-import { auth } from './firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { User } from './data/types';
 
 function AppContent() {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+      setCurrentUser(currentUser);
     });
 
     return () => unsubscribe();
