a488109d1ead6b67ddedea7d9662e0c74a5cc0e0
Kikkiflo <florentin.kristina@gmail.com>
2024-10-18 17:02:28 +0200

    grundstrukturen i NameAndAvatarSelection

--- components/NameAndAvatarSelection.tsx
+++ components/NameAndAvatarSelection.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { TextInput, Button, Text, useTheme } from 'react-native-paper';
 
 const NameAndAvatarSelection = () => {
+    const [name, setName] = useState('');
+    const { colors } = useTheme();
+
     return (
         <View style={styles.container}>
+            <TextInput
+                label="Ange ditt namn"
+                value={name}
+                onChangeText={setName}
+                mode="outlined"
+                style={styles.input}
+            />
+            <Button mode="contained" disabled={!name}>
+                Bekräfta
+            </Button>
         </View>
     );
 };
@@ -11,6 +25,9 @@ const NameAndAvatarSelection = () => {
 const styles = StyleSheet.create({
     container: {
         padding: 20,
+    },
+    input: {
+        marginBottom: 20,
     },
 });
 
