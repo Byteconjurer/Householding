b4445f241d063427d8d9a8f554612b5cfcde45be
Kikkiflo <florentin.kristina@gmail.com>
2024-10-22 10:10:50 +0200

    efter prettier:fix

--- components/JoinHouseholdModal.tsx
+++ components/JoinHouseholdModal.tsx
@@ -5,50 +5,50 @@ import JoinByCode from './JoinByCode';
 import NameAndAvatarSelection from './NameAndAvatarSelection';
 
 type JoinHouseholdModalProps = {
-    modalVisible: boolean;
-    setModalVisible: (visible: boolean) => void;
+  modalVisible: boolean;
+  setModalVisible: (visible: boolean) => void;
 };
 
 export default function JoinHouseholdModal({
-    modalVisible,
-    setModalVisible,
+  modalVisible,
+  setModalVisible,
 }: JoinHouseholdModalProps) {
-    const [householdId, setHouseholdId] = useState<string | null>(null);
+  const [householdId, setHouseholdId] = useState<string | null>(null);
 
-    const handleCodeValidated = (id: string) => {
-        setHouseholdId(id);
-    };
+  const handleCodeValidated = (id: string) => {
+    setHouseholdId(id);
+  };
 
-    const resetHouseholdId = () => {
-        setHouseholdId(null);
-    };
+  const resetHouseholdId = () => {
+    setHouseholdId(null);
+  };
 
-    return (
-        <Portal>
-            <Modal
-                visible={modalVisible}
-                onDismiss={() => setModalVisible(false)}
-                contentContainerStyle={styles.modalContainer}
-            >
-                {householdId === null ? (
-                    <JoinByCode onCodeValidated={handleCodeValidated} />
-                ) : (
-                    <NameAndAvatarSelection
-                        householdId={householdId}
-                        setModalVisible={setModalVisible}
-                        resetHouseholdId={resetHouseholdId}
-                    />
-                )}
-            </Modal>
-        </Portal>
-    );
+  return (
+    <Portal>
+      <Modal
+        visible={modalVisible}
+        onDismiss={() => setModalVisible(false)}
+        contentContainerStyle={styles.modalContainer}
+      >
+        {householdId === null ? (
+          <JoinByCode onCodeValidated={handleCodeValidated} />
+        ) : (
+          <NameAndAvatarSelection
+            householdId={householdId}
+            setModalVisible={setModalVisible}
+            resetHouseholdId={resetHouseholdId}
+          />
+        )}
+      </Modal>
+    </Portal>
+  );
 }
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        backgroundColor: '#fff',
-        padding: 20,
-        margin: 20,
-        borderRadius: 10,
-    },
+  modalContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    margin: 20,
+    borderRadius: 10,
+  },
 });
