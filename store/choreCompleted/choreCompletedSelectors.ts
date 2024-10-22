6ca4fe93ac446f256884b0737e0afa33f8b70718
Kristoffer Sundin <79krisun@skola.boras.se>
2024-10-22 16:56:36 +0200

    New, almost empty, file choreCompletedSelectors.ts

---
+++ store/choreCompleted/choreCompletedSelectors.ts
@@ -0,0 +1,6 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { selectCurrentHousehold } from '../household/householdSelectors';
+
+export const selectCompletedChores = (state: RootState) =>
+  state.choreCompleted.list;
