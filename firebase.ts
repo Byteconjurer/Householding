092ac1234e102e0f2e96153b8edb19d4d3bc9044
FridaUltra <5carlsonfrida@gmail.com>
2024-10-23 10:23:48 +0200

    Add native async storage so user still are  inlogged after reload

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +17,8 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-export const auth = initializeAuth(app);
+// export const auth = initializeAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
