c1df0ed6b757ceb1e501f2be958194e8c17677d4
Kristina Florentin <144032432+Kikkiflo@users.noreply.github.com>
2024-10-25 10:23:12 +0200

    98 fixa redux koppling till nameochavatarselection samt uppdatera avatarerna (#104)
    * uppdaterat att använda avatarsMap
    
    * added dispatch
    
    * återanvände selektorer från slice samt la till två för att hämta ut hushåll
    
    * finlir
    
    * prettier fix
    
    * remove chore.ts and fix selector SelectHouseHoldmember to get the list.
    
    * simplify id on add householmember
    
    ---------
    
    Co-authored-by: MattiasOtter <mattias__o@live.se>
    Co-authored-by: FridaUltra <5carlsonfrida@gmail.com>

--- store/householdmember/householdmemberSlice.ts
+++ store/householdmember/householdmemberSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { mockedHouseholdMembers } from '../../data/data';
 import { HouseholdMember } from '../../data/types';
 import { RootState } from '../store';
-import { mockedHouseholdMembers } from '../../data/data';
 
 type HouseholdMemberState = {
   list: HouseholdMember[];
@@ -60,4 +60,4 @@ export const {
 
 // SELECTORS
 export const selectHouseholdMembers = (state: RootState) =>
-  state.householdmember;
+  state.householdmember.list;
