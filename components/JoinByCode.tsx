cbc7898a880ce50d4c6d4864f60c46efbec79be4
Developmentator <v.svensson.89@gmail.com>
2024-10-30 19:17:45 +0100

    Changes Text import

--- components/JoinByCode.tsx
+++ components/JoinByCode.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Button, TextInput, Text } from 'react-native-paper';
 import { Household } from '../data/types';
 import {
   selectCurrentUser,
