6a3eaaaec7393a184c3dfe4f100f0d056602e437
Developmentator <v.svensson.89@gmail.com>
2024-11-01 17:31:53 +0100

    Sets the default theme color to system on app start, but allows for changing it in the app

--- App.tsx
+++ App.tsx
@@ -18,6 +18,7 @@ import { User } from './data/types';
 import { auth } from './firebase';
 import LoginStackNavigator from './navigators/LoginStackNavigator';
 import RootStackNavigator from './navigators/RootStackNavigator';
+import { Appearance } from 'react-native';
 import { ThemePreferencesContext } from './providers/ThemePreferencesContext';
 import { selectCurrentUser } from './store/sharedSelectors';
 import store, { useAppDispatch, useAppSelector } from './store/store';
@@ -69,13 +70,15 @@ function AppContent({ theme }: { theme: any }) {
 }
 
 export default function App() {
-  const [isThemeDark, setIsThemeDark] = useState(false);
+  const [isThemeDark, setIsThemeDark] = useState(
+    Appearance.getColorScheme() === 'dark',
+  );
 
   let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
 
   const toggleTheme = useCallback(() => {
-    return setIsThemeDark(!isThemeDark);
-  }, [isThemeDark]);
+    return setIsThemeDark((isDark) => !isDark);
+  }, []);
 
   const preferences = useMemo(
     () => ({
